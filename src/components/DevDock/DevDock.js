4463f634f006bdcc939ec0a73ffd3f4241f54b1d
SpyR1014 <spyr1014@gmail.com>
2017-03-29 17:59:54 +1100

    devDock can addTriplets

--- src/components/DevDock/DevDock.js
+++ src/components/DevDock/DevDock.js
@@ -8,23 +8,30 @@ import './devDock.css';
 import { addTriplet } from '../../actions/d3actions';
 
 const devDock = React.createClass({
+    getInitialState: function(){
+        return {
+            subject: "",
+            predicate: "",
+            object: ""
+        }
+    },
     render: function(){
         return <div id="devDock">
             <div>
                 <div>
                     <label htmlFor="subject">Subject:</label>
-                    <input id="subject" value="" />
+                    <input id="subject" value={this.state.subject} onChange={(e) => {this.setState({subject: e.target.value})}} />
                 </div>
                 <div>
                     <label htmlFor="predicate">Edge:</label>
-                    <input id="predicate" value="" />
+                    <input id="predicate" value={this.state.predicate} onChange={(e) => {this.setState({predicate: e.target.value})}} />
                 </div>
                 <div>
                     <label htmlFor="object">Object:</label>
-                    <input id="object" value="" />
+                    <input id="object" value={this.state.object} onChange={(e) => {this.setState({object: e.target.value})}} />
                 </div>
             </div>
-            <button onClick={() => {this.props.addTriplet("a", 'rar', 'c')}}>Add Triplet</button>
+            <button onClick={() => {this.props.addTriplet(this.state.subject, this.state.predicate, this.state.object)}}>Add Triplet</button>
         </div>
     }
 });
