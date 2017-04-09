730ae25cf80af5dcbcd4889099fcd6256de776dc
SpyR1014 <spyr1014@gmail.com>
2017-04-09 16:31:50 +1000

    Added sources type dragonfly css

--- src/components/DragonflyTypeList/DragonflyTypeList.js
+++ src/components/DragonflyTypeList/DragonflyTypeList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import "./dragonflytypelist.css";
+
 const DragonflyTypeList = (props) => {
     if (!props.type){
         console.error("No props.type")
@@ -10,7 +12,7 @@ const DragonflyTypeList = (props) => {
     if (!props.clickHandler){
         console.error("No props.clickHandler")
     }
-    return (<li key={props.type}>
+    return (<li key={props.type} className="typeList">
                     {props.type}
                     <ul>{
                         props.nodes.map(v => <li key={v.hash}
